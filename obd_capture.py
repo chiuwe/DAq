bec82480b1cdf707feded06d640a64825c33273b
Steven Chiu <steven.y.chiu@gmail.com>
2016-06-04 20:30:25 -0700

    cleaned up code with debug print out method

--- obd_capture.py
+++ obd_capture.py
@@ -2,40 +2,44 @@ import obd
 import time
 import csv
 import smbus
+import MMA8451
+import gps
 from datetime import datetime
 
 MAX_SUPPORTED_COMMANDS = 52
 KPH_TO_MPH = 0.621371
+DEBUG = False
+
 connection = None
+
+def debug(str):
+	if DEBUG:
+		print str
 
 def init_connection():
 	global connection
 	while True:
 		try:
 			connection = obd.Async()
-			print "connected"
+			debug("connected")
 			if len(connection.supported_commands) >= MAX_SUPPORTED_COMMANDS:
-				print "passed"
+				debug("passed")
 				connection.watch(obd.commands.ENGINE_LOAD)
-				print "ENGINE_LOAD"
+				debug("ENGINE_LOAD")
 				connection.watch(obd.commands.COOLANT_TEMP)
-				print "COOLANT_TEMP"
+				debug("COOLANT_TEMP")
 				connection.watch(obd.commands.RPM)
-				print "RPM"
+				debug("RPM")
 				connection.watch(obd.commands.SPEED)
-				print "SPEED"
+				debug("SPEED")
 				connection.watch(obd.commands.INTAKE_TEMP)
-				print "INTAKE_TEMP"
+				debug("INTAKE_TEMP")
 				connection.watch(obd.commands.MAF)
-				print "MAF"
+				debug("MAF")
 				connection.watch(obd.commands.THROTTLE_POS)
-				print "THROTTLE_POS"
-# 				connection.watch(obd.commands.OIL_TEMP)
-# 				print "OIL_TEMP"
-# 				connection.watch(obd.commands.FUEL_RATE)
-# 				print "FUEL_RATE"
+				debug("THROTTLE_POS")
 				connection.start()
-				print "OBD watchdog started!"
+				debug("OBD watchdog started!")
 				break
 			connection.close()
 			time.sleep(1)
@@ -47,7 +51,7 @@ def init_connection():
 def log_data():
 	filename = time.strftime("%Y%m%d%H%M.csv")
 	
-	print filename
+	debug(filename)
 	
 	with open(filename, 'w') as csvfile:
 		fieldnames = ['time', 'engine_load', 'coolant_temp', 'rpm', 'speed', 'intake_temp', 'maf', 'throttle_pos']
@@ -64,42 +68,14 @@ def log_data():
 				'speed': (connection.query(obd.commands.SPEED).value * KPH_TO_MPH),
 				'intake_temp': connection.query(obd.commands.INTAKE_TEMP).value,
 				'maf': connection.query(obd.commands.MAF).value,
-				'throttle_pos': connection.query(obd.commands.THROTTLE_POS).value,
-# 				'oil_temp': connection.query(obd.commands.OIL_TEMP).value,
-# 				'fuel_rate': connection.query(obd.commands.FUEL_RATE).value
-				})
+				'throttle_pos': connection.query(obd.commands.THROTTLE_POS).value})
 			time.sleep(0.1)
 	
 	connection.stop()
 	connection.close()
 		
 if __name__ == "__main__":
-	
-	print "hello world!"
+
 	while True:
 		init_connection()
 		log_data()
-# what do i name each file thats created?
-# timestamp of when it first successfully connected. use GPS to get relative position racetrack name if possible.
-
-
-# how do i know when to start and stop logging?
-# it should auto start, when obd.status() is lost, stop logging and close write file if needed, go back to searching.
-
-# 04 ENGINE_LOAD
-# 05 COOLANT_TEMP
-# 0C RPM
-# 0D SPEED
-# 0F INTAKE_TEMP
-# 10 MAF
-# 11 THROTTLE_POS
-# 5C OIL_TEMP
-
-# with open('names.csv', 'w') as csvfile:
-#     fieldnames = ['first_name', 'last_name']
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-# 
-#     writer.writeheader()
-#     writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-#     writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-#     writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
