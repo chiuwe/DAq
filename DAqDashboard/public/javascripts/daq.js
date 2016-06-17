d030b4a2d5253d764bfe7de5dbe8b3bdec2eceda
Steven Chiu <steven.y.chiu@gmail.com>
2016-06-17 15:27:14 -0700

    creates laps in data2

--- DAqDashboard/public/javascripts/daq.js
+++ DAqDashboard/public/javascripts/daq.js
@@ -3,6 +3,25 @@ function getSelectedValue() {
 	return "/csv/" + file;
 }
 
+function CCW(p1, p2, p3) {
+  a = p1.lon; b = p1.lat; 
+  c = p2.lon; d = p2.lat;
+  e = p3.lon; f = p3.lat;
+  return (f - b) * (c - a) > (d - b) * (e - a);
+}
+
+function isIntersect(p1, p2, p3, p4) {
+  return (CCW(p1, p3, p4) != CCW(p2, p3, p4)) && (CCW(p1, p2, p3) != CCW(p1, p2, p4));
+}
+
+function point(lon, lat) {
+  this.lon = lon;
+  this.lat = lat;
+}
+
+// 36.586491, -121.756684
+// 36.586425, -121.756568
+
 var dataPoints = [
 	{type: "engineLoad", name: "Engine Load", unit: "%"},
 	{type: "coolantTemp", name: "Coolant Temp", unit: "°C"},
@@ -31,38 +50,130 @@ var MARGINS = {
 	left: 80
 };
 var data;
+var data2;
 render();
 function render() {
-var file = getSelectedValue();
-d3.selectAll("svg").remove();
-//console.log(file);
-d3.csv(file)
-	.row(function(d) {return {
-		time: timeFormat.parse(d.time.replace(/(\.[0-9]{3})[0-9]*/, "$1")),
-		engineLoad: +d.engineLoad,
-		coolantTemp: +d.coolantTemp,
-		rpm: +d.rpm,
-		speed: +d.speed,
-		intakeTemp: +d.intakeTemp,
-		maf: +d.maf,
-		throttlePos: +d.throttlePos,
-		timingAdvance: +d.timingAdvance,
-		xG: +d.xG,
-		yG: +d.yG,
-		zG: +d.zG,
-		gpsSpeed: +d.gpsSpeed,
-		gpsLat: +d.gpsLat,
-		gpsLon: +d.gpsLon,
-		gpsAlt: +d.gpsAlt,
-		gpsClimb: +d.gpsClimb};
-	})
-	.get(function(error, rows) {
-		data = rows;
-		processData();
-	});
+	var file = getSelectedValue();
+	d3.selectAll("svg").remove();
+	d3.csv(file)
+		.row(function(d) {return {
+			time: timeFormat.parse(d.time.replace(/(\.[0-9]{3})[0-9]*/, "$1")),
+			engineLoad: +d.engineLoad,
+			coolantTemp: +d.coolantTemp,
+			rpm: +d.rpm,
+			speed: +d.speed,
+			intakeTemp: +d.intakeTemp,
+			maf: +d.maf,
+			throttlePos: +d.throttlePos,
+			timingAdvance: +d.timingAdvance,
+			xG: +d.xG,
+			yG: +d.yG,
+			zG: +d.zG,
+			gpsSpeed: +d.gpsSpeed,
+			gpsLat: +d.gpsLat,
+			gpsLon: +d.gpsLon,
+			gpsAlt: +d.gpsAlt,
+			gpsClimb: +d.gpsClimb};
+		})
+		.get(function(error, rows) {
+			data = rows;
+			processLaps();
+			processData();
+		});
 
-function processData() {
-	for (x in dataPoints) {
+	function processLaps() {
+		lap = 0;
+		temp = [];
+		data2 = [];
+		p1 = new point(-121.756684, 36.586491);
+		p2 = new point(-121.756568, 36.586425);
+		for (x = 0; x < data.length - 1; x++) {
+			p3 = new point(data[x].gpsLon, data[x].gpsLat);
+			p4 = new point(data[x+1].gpsLon, data[x+1].gpsLat);
+			temp.push(data[x])
+			if (isIntersect(p1, p2, p3, p4)) {
+				data2[lap] = temp;
+				lap++;
+				temp = [];
+			}
+		}
+	}
+
+	function processData() {
+		for (x in dataPoints) {
+			var svg = d3.select("main").append("svg")
+				.attr("width", width + MARGINS.left + MARGINS.right)
+				.attr("height", height + MARGINS.top + MARGINS.bottom);
+
+			// X-axis
+			var xScale = d3.time.scale().domain([data[0].time, data[data.length-1].time]).range([MARGINS.left, width + MARGINS.left - MARGINS.right]);
+			xScale.tickFormat(d3.time.format("%H:%M:%S.%L"));
+			var xAxis = d3.svg.axis().scale(xScale).ticks(6);
+			svg.append("text")
+				.attr("x", width / 2)
+				.attr("y", height + MARGINS.bottom)
+				.style("text-anchor", "middle")
+				.text("Time");
+			svg.append("svg:g")
+				.attr('stroke-width', 1)
+				.attr('transform', 'translate(0,' + (height - MARGINS.bottom) + ')')
+				.call(xAxis);
+
+			// Y-axis
+			var yScale = generateYAxis(dataPoints[x].type);
+			var yAxis = d3.svg.axis().scale(yScale).orient("left");
+			svg.append("text")
+				.attr("transform", "rotate(-90)")
+				.attr("x", 0 - (height / 2))
+				.attr("dy", "1em")
+				.style("text-anchor", "middle")
+				.text(dataPoints[x].name + " (" + dataPoints[x].unit + ")");
+			svg.append("svg:g")
+				.attr('transform', 'translate(' + MARGINS.left + ',0)')
+				.call(yAxis);
+
+			// Line
+			var lineFunc = d3.svg.line()
+				.x(function(d) {
+					return xScale(d["time"]);
+				})
+				.y(function(d) {
+					return yScale(d[dataPoints[x].type]);
+				});
+			var line = svg.append('svg:path')
+				.attr('d', lineFunc(data))
+				.attr("class", "line");
+		}
+
+		// G-force graph
+		var gForce = d3.select("main").append("svg")
+			.attr("width", 500)
+			.attr("height", 500);
+		var xScale = d3.scale.linear().domain([-2,2]).range([0,500]);
+		var xAxis = d3.svg.axis().scale(xScale).ticks(10);
+		gForce.append("svg:g")
+			.attr('stroke-width', 1)
+			.attr('transform', 'translate(0,250)')
+			.call(xAxis);
+		var yScale = d3.scale.linear().domain([-2,2]).range([500,0]);
+		var yAxis = d3.svg.axis().scale(yScale).orient("left");
+		gForce.append("svg:g")
+			.attr('transform', 'translate(250,0)')
+			.call(yAxis);
+		var lineFunc = d3.svg.line()
+			.x(function(d) {
+				return xScale(d["xG"]);
+				})
+			.y(function(d) {
+				return yScale(d["yG"]);
+			});
+		gForce.append("svg:path")
+			.attr("d", lineFunc(data))
+			.attr("stroke", "blue")
+			.attr("stroke-width", 1)
+			.attr("fill", "none");
+	
+		// OBD Speed vs GPS Speed
 		var svg = d3.select("main").append("svg")
 			.attr("width", width + MARGINS.left + MARGINS.right)
 			.attr("height", height + MARGINS.top + MARGINS.bottom);
@@ -82,144 +193,71 @@ function processData() {
 			.call(xAxis);
 
 		// Y-axis
-		var yScale = generateYAxis(dataPoints[x].type);
+		var yScale = generateYAxis('speed');
 		var yAxis = d3.svg.axis().scale(yScale).orient("left");
 		svg.append("text")
 			.attr("transform", "rotate(-90)")
 			.attr("x", 0 - (height / 2))
 			.attr("dy", "1em")
 			.style("text-anchor", "middle")
-			.text(dataPoints[x].name + " (" + dataPoints[x].unit + ")");
+			.text("OBD vs GPS Speed (mph)");
 		svg.append("svg:g")
 			.attr('transform', 'translate(' + MARGINS.left + ',0)')
 			.call(yAxis);
 
-		// Line
-		var lineFunc = d3.svg.line()
+		// OBD Speed Line
+		var OBDLineFunc = d3.svg.line()
 			.x(function(d) {
 				return xScale(d["time"]);
 			})
 			.y(function(d) {
-				return yScale(d[dataPoints[x].type]);
+				return yScale(d['speed']);
 			});
-		var line = svg.append('svg:path')
-			.attr('d', lineFunc(data))
+		var OBDLine = svg.append('svg:path')
+			.attr('d', OBDLineFunc(data))
 			.attr("class", "line");
-	}
-
-	// G-force graph
-	var gForce = d3.select("main").append("svg")
-		.attr("width", 500)
-		.attr("height", 500);
-	var xScale = d3.scale.linear().domain([-2,2]).range([0,500]);
-	var xAxis = d3.svg.axis().scale(xScale).ticks(10);
-	gForce.append("svg:g")
-		.attr('stroke-width', 1)
-		.attr('transform', 'translate(0,250)')
-		.call(xAxis);
-	var yScale = d3.scale.linear().domain([-2,2]).range([500,0]);
-	var yAxis = d3.svg.axis().scale(yScale).orient("left");
-	gForce.append("svg:g")
-		.attr('transform', 'translate(250,0)')
-		.call(yAxis);
-	var lineFunc = d3.svg.line()
-		.x(function(d) {
-			return xScale(d["xG"]);
+	
+		// GPS Speed Line
+		var gpsLineFunc = d3.svg.line()
+			.x(function(d) {
+				return xScale(d["time"]);
 			})
-		.y(function(d) {
-			return yScale(d["yG"]);
-		});
-	gForce.append("svg:path")
-		.attr("d", lineFunc(data))
-		.attr("stroke", "blue")
-		.attr("stroke-width", 1)
-		.attr("fill", "none");
+			.y(function(d) {
+				return yScale(d['gpsSpeed']);
+			});
+		var gpsLine = svg.append('svg:path')
+			.attr('d', gpsLineFunc(data))
+			.attr("class", "line blue");
 	
-	// OBD Speed vs GPS Speed
-	var svg = d3.select("main").append("svg")
-		.attr("width", width + MARGINS.left + MARGINS.right)
-		.attr("height", height + MARGINS.top + MARGINS.bottom);
-
-	// X-axis
-	var xScale = d3.time.scale().domain([data[0].time, data[data.length-1].time]).range([MARGINS.left, width + MARGINS.left - MARGINS.right]);
-	xScale.tickFormat(d3.time.format("%H:%M:%S.%L"));
-	var xAxis = d3.svg.axis().scale(xScale).ticks(6);
-	svg.append("text")
-		.attr("x", width / 2)
-		.attr("y", height + MARGINS.bottom)
-		.style("text-anchor", "middle")
-		.text("Time");
-	svg.append("svg:g")
-		.attr('stroke-width', 1)
-		.attr('transform', 'translate(0,' + (height - MARGINS.bottom) + ')')
-		.call(xAxis);
-
-	// Y-axis
-	var yScale = generateYAxis('speed');
-	var yAxis = d3.svg.axis().scale(yScale).orient("left");
-	svg.append("text")
-		.attr("transform", "rotate(-90)")
-		.attr("x", 0 - (height / 2))
-		.attr("dy", "1em")
-		.style("text-anchor", "middle")
-		.text("OBD vs GPS Speed (mph)");
-	svg.append("svg:g")
-		.attr('transform', 'translate(' + MARGINS.left + ',0)')
-		.call(yAxis);
-
-	// OBD Speed Line
-	var OBDLineFunc = d3.svg.line()
-		.x(function(d) {
-			return xScale(d["time"]);
-		})
-		.y(function(d) {
-			return yScale(d['speed']);
-		});
-	var OBDLine = svg.append('svg:path')
-		.attr('d', OBDLineFunc(data))
-		.attr("class", "line");
+		// Tooltip
+		var tooltip = svg.append("g")
+			.attr("class", "tooltip")
+			.style("display", "none");
+		tooltip.append("circle")
+			.attr("fill", "red")
+			.attr("r", 3);
+		tooltip.append("text")
+			.attr("x", 7)
+			.attr("dy", ".35em");
+		svg.append("rect")
+			.attr("class", "overlay")
+			.attr("width", width + MARGINS.left + MARGINS.right)
+			.attr("height", height + MARGINS.top + MARGINS.bottom)
+			.on("mouseover", function() { tooltip.style("display", null); })
+			.on("mouseout", function () { tooltip.style("display", "none"); })
+			.on("mousemove", mousemove);
 	
-	// GPS Speed Line
-	var gpsLineFunc = d3.svg.line()
-		.x(function(d) {
-			return xScale(d["time"]);
-		})
-		.y(function(d) {
-			return yScale(d['gpsSpeed']);
-		});
-	var gpsLine = svg.append('svg:path')
-		.attr('d', gpsLineFunc(data))
-		.attr("class", "line blue");
-	
-	// Tooltip
-	var tooltip = svg.append("g")
-		.attr("class", "tooltip")
-		.style("display", "none");
-	tooltip.append("circle")
-		.attr("fill", "red")
-		.attr("r", 3);
-	tooltip.append("text")
-		.attr("x", 7)
-		.attr("dy", ".35em");
-	svg.append("rect")
-		.attr("class", "overlay")
-		.attr("width", width + MARGINS.left + MARGINS.right)
-		.attr("height", height + MARGINS.top + MARGINS.bottom)
-		.on("mouseover", function() { tooltip.style("display", null); })
-		.on("mouseout", function () { tooltip.style("display", "none"); })
-		.on("mousemove", mousemove);
-	
-	var bisectTime = d3.bisector(function(d) { return d.time}).left;
-	function mousemove() {
-		var xPos = xScale.invert(d3.mouse(this)[0]);
-		var i = bisectTime(data, xPos);
-		var d0 = data[i-1];
-		var d1 = data[i];
-		var d = xPos - d0.time > d1.time - xPos ? d1 : d0;
-		tooltip.attr("transform", "translate(" + xScale(d.time) + "," + yScale(d['gpsSpeed']) + ")");
-		tooltip.select("text").text(d['gpsSpeed']);
+		var bisectTime = d3.bisector(function(d) { return d.time}).left;
+		function mousemove() {
+			var xPos = xScale.invert(d3.mouse(this)[0]);
+			var i = bisectTime(data, xPos);
+			var d0 = data[i-1];
+			var d1 = data[i];
+			var d = xPos - d0.time > d1.time - xPos ? d1 : d0;
+			tooltip.attr("transform", "translate(" + xScale(d.time) + "," + yScale(d['gpsSpeed']) + ")");
+			tooltip.select("text").text(d['gpsSpeed']);
+		}
 	}
-}
 }
 
 function generateYAxis(dataPoint) {
