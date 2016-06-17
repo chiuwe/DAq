014460fc4907e39177e7963c67a329b35a28c091
Steven Chiu <steven.y.chiu@gmail.com>
2016-06-16 23:28:11 -0700

    3 line graph

--- MMAGraphTest.py
+++ MMAGraphTest.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from collections import deque
 
+TAILLEN = 2
+
 # Plot Class
 class AnalogPlot:
 	# constructor
@@ -28,18 +30,22 @@ class AnalogPlot:
 		self.addToBuf(self.az, z)
 
 	# update plot
-	def update(self, frameNum, a0):
+	def update(self, frameNum, a0, a1, a2):
 		x, y, z = self.accel.readScaledData()
 		self.add(x, y, z)
 		a0.set_data(self.ax, self.ay)
-# 		a1.set_data(range(self.maxLen), self.ay)	
-
+		a1.set_data(self.ax, self.az)
+		a2.set_data(self.ay, self.az)
+		
 if __name__ == '__main__':
 	mma = mma.MMA8451()
 	mma.setup()
-	analogPlot = AnalogPlot(mma, 10)
+	analogPlot = AnalogPlot(mma, TAILLEN)
 	fig = plt.figure()
 	ax = plt.axes(xlim=(-2, 2), ylim=(-2, 2))
-	a0 = ax.plot([], [])
-	anim = animation.FuncAnimation(fig, analogPlot.update, fargs=a0, interval=1)
+	a0, = ax.plot([], [], label='XY')
+	a1, = ax.plot([], [], label='XZ')
+	a2, = ax.plot([], [], label='YZ')
+	plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand", borderaxespad=0.)
+	anim = animation.FuncAnimation(fig, analogPlot.update, fargs=(a0, a1, a2), interval=1)
 	plt.show()
